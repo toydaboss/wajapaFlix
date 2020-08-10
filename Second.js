a1db9de578f4f6c00587f472d380b6833715f740
intrepidtoju <61869083+intrepidtoju@users.noreply.github.com>
2020-08-10 12:53:30 +0100

    Create Second.js

---
+++ Second.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Mobile from "../../images/mobile-0819.jpg";
+
+function Second() {
+  return (
+    <div className="section-two">
+    <div>
+        <img src={Mobile} alt="" srcset=""/>
+    </div>
+    <div>
+        <h2>Download your shows to watch offline.</h2>
+        <p>Save your favorites easily and always have <br></br> something to watch.</p>
+    </div>
+</div>
+  );
+}
+
+export default Second;
